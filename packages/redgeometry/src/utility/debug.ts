cfa6a2290655b74262240a13bb3295899ee3a561
yzrmn <yzrmn@users.noreply.github.com>
2025-03-31 13:05:53 +0200

    Add more validation helper methods

--- packages/redgeometry/src/utility/debug.ts
+++ packages/redgeometry/src/utility/debug.ts
@@ -21,14 +21,42 @@ export class ValidationHelper {
 
     public equal<T>(valueA: T, valueB: T, fmt: string, ...params: FormatParameters): void {
         if (valueA !== valueB) {
-            const message = `${formatString(fmt, ...params)}: ${valueA} expected to be EQUAL to ${valueB}`;
+            const message = `${formatString(fmt, ...params)}: '${valueA}' expected to be EQUAL to '${valueB}'`;
+            this.errors.push(message);
+        }
+    }
+
+    public greaterThan<T>(valueA: T, valueB: T, fmt: string, ...params: FormatParameters): void {
+        if (valueA <= valueB) {
+            const message = `${formatString(fmt, ...params)}: '${valueA}' expected to be GREATER THAN '${valueB}'`;
+            this.errors.push(message);
+        }
+    }
+
+    public greaterThanOrEqual<T>(valueA: T, valueB: T, fmt: string, ...params: FormatParameters): void {
+        if (valueA < valueB) {
+            const message = `${formatString(fmt, ...params)}: '${valueA}' expected to be GREATER THAN OR EQUAL to'${valueB}'`;
+            this.errors.push(message);
+        }
+    }
+
+    public lessThan<T>(valueA: T, valueB: T, fmt: string, ...params: FormatParameters): void {
+        if (valueA >= valueB) {
+            const message = `${formatString(fmt, ...params)}: '${valueA}' expected to be LESS THAN '${valueB}'`;
+            this.errors.push(message);
+        }
+    }
+
+    public lessThanOrEqual<T>(valueA: T, valueB: T, fmt: string, ...params: FormatParameters): void {
+        if (valueA > valueB) {
+            const message = `${formatString(fmt, ...params)}: '${valueA}' expected to be LESS THAN OR EQUAL to '${valueB}'`;
             this.errors.push(message);
         }
     }
 
     public notEqual<T>(valueA: T, valueB: T, fmt: string, ...params: FormatParameters): void {
         if (valueA === valueB) {
-            const message = `${formatString(fmt, ...params)}: ${valueA} expected to be NOT EQUAL to ${valueB}`;
+            const message = `${formatString(fmt, ...params)}: '${valueA}' expected to be NOT EQUAL to '${valueB}'`;
             this.errors.push(message);
         }
     }
