e399cbf4f1d8e9ba6ab12f82ccedab7047e7ae9c
yzrmn <yzrmn@users.noreply.github.com>
2026-03-27 23:28:19 +0000

    Update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,11 @@
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({
+export default defineConfig({
     files: ["**/{src,tests}/**/*.ts"],
-    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    extends: [eslint.configs.recommended, tseslint.configs.recommended],
     languageOptions: {
-        parser: tseslint.parser,
         parserOptions: {
             projectService: true,
         },
@@ -13,14 +13,7 @@ export default tseslint.config({
     rules: {
         eqeqeq: "error",
         "no-useless-assignment": "off",
-        "@typescript-eslint/no-explicit-any": [
-            "error",
-            {
-                ignoreRestArgs: true,
-            },
-        ],
         "@typescript-eslint/no-unnecessary-condition": "error",
-        "@typescript-eslint/no-unused-expressions": "error",
         "@typescript-eslint/no-unused-vars": [
             "error",
             {
