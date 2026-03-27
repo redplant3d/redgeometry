616e887d5272c39761ce832727aa7c6b5d0dd5d5
yzrmn <yzrmn@users.noreply.github.com>
2026-03-27 10:57:44 +0100

    Use relative base path for deployment

--- packages/redgeometry-app/vite.config.ts
+++ packages/redgeometry-app/vite.config.ts
@@ -1,6 +1,7 @@
 import { type ConfigEnv, type UserConfigExport } from "vite";
 
 const config: UserConfigExport = (env: ConfigEnv) => ({
+    base: "./",
     define: {
         REDGEOMETRY_DEBUG: env.mode === "development",
     },
