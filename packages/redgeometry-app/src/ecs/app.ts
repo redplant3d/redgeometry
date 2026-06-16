bd73a953bd5dbfc68463d16374b6173d6cb8ea1b
yzrmn <yzrmn@users.noreply.github.com>
2026-06-16 16:40:09 +0200

    Simplify ECS app code

--- packages/redgeometry-app/src/ecs/app.ts
+++ packages/redgeometry-app/src/ecs/app.ts
@@ -1,16 +1,5 @@
-import { assertDebug, log, throwError } from "redgeometry/src/utility/debug";
-import {
-    LocalAppRemote,
-    WorldChannelLocal,
-    WorldChannelRemote,
-    WorldGroup,
-    type AppMessageRequestData,
-    type AppMessageRequestFn,
-    type AppMessageResponseData,
-    type AppMessageResponseFn,
-    type WorldGroupChild,
-    type WorldGroupParent,
-} from "../utility/ecs-app.ts";
+import { log } from "redgeometry/src/utility/debug";
+import { WorldChannelLocal, WorldGroup } from "../utility/ecs-app.ts";
 import type {
     DefaultSystemStage,
     DefaultWorldScheduleId,
@@ -42,31 +31,6 @@ export interface AppContext {
     readonly isMain: boolean;
     readonly isWorker: boolean;
     readonly selfName: string | undefined;
-
-    createRemoteChild(
-        requestSenderId: WorldGroupId,
-        requestReceiverId: WorldGroupId,
-        scriptURL: URL | string | undefined,
-    ): AppRemoteChild;
-    createRemoteParent(responseSenderId: WorldGroupId, responseReceiverId: WorldGroupId): AppRemoteParent;
-    isValidGroup(groupOptions: WorldGroupOptions): boolean;
-}
-
-export interface AppRemoteChild {
-    readonly requestReceiverId: WorldGroupId;
-    readonly requestSenderId: WorldGroupId;
-
-    sendRequest(data: AppMessageRequestData, transfer: Transferable[]): void;
-    sendRequestAsync(data: AppMessageRequestData, transfer: Transferable[]): Promise<void>;
-    setReceiveResponseFn(fn: AppMessageResponseFn): void;
-}
-
-export interface AppRemoteParent {
-    readonly responseReceiverId: WorldGroupId;
-    readonly responseSenderId: WorldGroupId;
-
-    sendResponse(requestMessageId: number, data: AppMessageResponseData[], transfer: Transferable[]): void;
-    setReceiveRequestFn(fn: AppMessageRequestFn): void;
 }
 
 export class App {
@@ -82,11 +46,7 @@ export class App {
     }
 
     public addWorldGroup(options: WorldGroupOptions): void {
-        const { id, parent } = options;
-
-        if (parent !== undefined && !this.groupOptionsMap.has(parent)) {
-            throwError("Unable to find parent '{}'", parent);
-        }
+        const { id } = options;
 
         if (this.groupOptionsMap.has(id)) {
             log.warn("Group '{}' will be overwritten", id);
@@ -100,16 +60,9 @@ export class App {
 
         // Create world groups
         for (const groupOptions of this.groupOptionsMap.values()) {
-            if (!context.isValidGroup(groupOptions)) {
-                continue;
-            }
+            const channels = this.createChannels(groupOptions);
 
-            const channels = this.createChannels(context, groupOptions);
-            const children = this.createChildren(context, groupOptions);
-            const parent = this.createParent(context, groupOptions);
-
-            const group = new WorldGroup(groupOptions.id, context, channels, children, parent);
-            group.init();
+            const group = new WorldGroup(groupOptions.id, context, channels);
 
             this.groupMap.set(groupOptions.id, group);
         }
@@ -124,7 +77,7 @@ export class App {
         }
     }
 
-    private createChannels(context: AppContext, groupOptions: WorldGroupOptions): Map<WorldId, WorldChannelLocal> {
+    private createChannels(groupOptions: WorldGroupOptions): Map<WorldId, WorldChannelLocal> {
         const channels = new Map<WorldId, WorldChannelLocal>();
 
         for (const worldOptions of groupOptions.worlds) {
@@ -141,56 +94,9 @@ export class App {
 
         return channels;
     }
-
-    private createChildren(context: AppContext, groupOptions: WorldGroupOptions): WorldGroupChild[] {
-        const children: WorldGroupChild[] = [];
-
-        for (const childGroupOptions of this.groupOptionsMap.values()) {
-            if (childGroupOptions.parent !== groupOptions.id) {
-                continue;
-            }
-
-            const remote = context.createRemoteChild(
-                groupOptions.id,
-                childGroupOptions.id,
-                childGroupOptions.workerScriptURL,
-            );
-            const channelMap = new Map<WorldId, WorldChannelRemote>();
-
-            for (const worldOptions of childGroupOptions.worlds) {
-                const channelRemote = new WorldChannelRemote(worldOptions.id, remote);
-                channelMap.set(worldOptions.id, channelRemote);
-            }
-
-            children.push({ remote, channelMap });
-        }
-
-        return children;
-    }
-
-    private createParent(context: AppContext, groupOptions: WorldGroupOptions): WorldGroupParent | undefined {
-        if (groupOptions.parent === undefined) {
-            return undefined;
-        }
-
-        const parentGroupOption = this.groupOptionsMap.get(groupOptions.parent);
-        assertDebug(parentGroupOption !== undefined);
-
-        const remote = context.createRemoteParent(groupOptions.id, parentGroupOption.id);
-        const channelMap = new Map<WorldId, WorldChannelRemote>();
-
-        for (const worldOptions of parentGroupOption.worlds) {
-            const channelRemote = new WorldChannelRemote(worldOptions.id, undefined);
-            channelMap.set(worldOptions.id, channelRemote);
-        }
-
-        return { remote, channelMap };
-    }
 }
 
 export class LocalAppContext implements AppContext {
-    private map: Map<WorldGroupId, LocalAppRemote>;
-
     public readonly isMain: boolean;
     public readonly isWorker: boolean;
     public readonly selfName: string | undefined;
@@ -203,31 +109,5 @@ export class LocalAppContext implements AppContext {
         this.isMain = isMain;
         this.isWorker = isWorker;
         this.selfName = selfName;
-
-        this.map = new Map();
-    }
-
-    public createRemoteChild(
-        requestSenderId: WorldGroupId,
-        requestReceiverId: WorldGroupId,
-        _scriptURL: URL | string | undefined,
-    ): AppRemoteChild {
-        const remote = new LocalAppRemote(requestSenderId, requestReceiverId);
-        this.map.set(requestReceiverId, remote);
-        return remote;
-    }
-
-    public createRemoteParent(responseSenderId: WorldGroupId, _responseReceiverId: WorldGroupId): AppRemoteParent {
-        const parentRemote = this.map.get(responseSenderId);
-
-        if (parentRemote === undefined) {
-            throwError("Unable to find parent remote");
-        }
-
-        return parentRemote;
-    }
-
-    public isValidGroup(_groupOptions: WorldGroupOptions): boolean {
-        return true;
     }
 }
